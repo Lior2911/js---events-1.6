6699c84a43890476b4202cbcb3bd7099efbf524d
lior2911 <aytegev1@gmail.com>
2022-06-07 09:02:42 +0300

    cmt

--- main.js
+++ main.js
@@ -64,13 +64,111 @@
 //   document.getElementById("h1_").style="color:green"
 // }
 
+
 // 9.	צרו תוכנית לוטו, התוכנית מקבלת מהמשתמש מספר בין 0 ל - 56 ובעת לחיצה על כפתור מדפיסה אותו למסך, הפעולה קורית 5 פעמים. לאחר מכן, התוכנית מגרילה מספר בין 0 ל 56 ומדפיסה אותו למסך.
+// var numArray = [];
 
-function getMoney(){
-  var lottoArray = []
+// function mainFunc(){
+//   var numInput = document.getElementById("user_number");
+//   var p_num = document.getElementById("some_p");
+//   if(numArray.length<5){
+//      p_num.innerText += "number : " + numInput.value + "\n" ;
+//      numArray.push(numInput.Value);
+//   }
+//   else{
+//    numInput.disabeled = true;
+//   }
+ 
+// }
+
+// function getAndPrint(){
+//   var rndNum = Math.floor(Math.random()*57);
+//   var rndH3 = document.getElementById("h3_lotto");
+//   rndH3.innerText += rndNum;
+//   for (var i = 0 ; i < numArray.length; i++){
+//     if(rndNum == numArray[i]){
+//       document.getElementById("result").innerText = "you win "
+//       return ;
+//     }
+//   }
+//   document.getElementById("result").innerText = "you lose"
+// }
+
+// 12/
+
+// var arrayNum = [12,43,5,667,564,34,234,5,67,9]
+// for (var i = 0 ; i <arrayNum.length ; i++){
+// main_div.innerHTML += "<div id= 'div_ " + i +" '>"+arrayNum[i]+"</div>";
+// changeColor(i,arrayNum);
   
-  for (var i = 0 ; i<5;i++){
-   var numFromUser = +prompt("enter number between 0-56")
+// }
+// function changeColor(index,arrayNum){
+//   if(arrayNum[index]%2==0){
+//     document.getElementById("div_"+index).style.color = "red"
+//   }
+//   else{
+//     document.getElementById("div_"+index).style.color = "blue"
 
- }
+//   }
+// }
+
+
+
+
+// 1.	צרו תוכנית שבעת עלייה מקבלת מהמשתמש שם ותאריך לידה, אם המשתמש מעל 18 מדפיסה למסך הודעה של ברוכים הבאים למסך בכחול ואת שם המשתמש אחרת הודעת אין כניסה באדום עם גיף מתאים( *אתגר - ומרעננת את הדף).
+// part1
+
+function getName (){
+  var inputName = document.getElementById("inputUserName");
+  return inputName.value;
 }
+function getAge(){
+  var inputAge = document.getElementById("inputUserAge");
+  return inputAge.value;
+}
+function getIn(){
+  document.getElementById("h1_").innerText = "welcome";
+  document.getElementById("h1_").style = "color:blue";
+}
+function getOut(){
+  document.getElementById("h1_").innerText = "go home";
+  document.getElementById("h1_").style = "color : red";
+  document.body.div.innerHTML = "<img src='https://i.makeagif.com/media/7-23-2015/cxjCsF.gif'>"
+}
+function getGreen(){
+  document.getElementById("h1_").innerText = "welcome";
+  document.getElementById("h1_").style = "color:green";
+}
+
+function enter(){
+ var myDate = new Date("01/01/2004");
+ return myDate;
+ 
+}
+function mainFunc (){
+  if(getAge()>"2004-01-01"){
+    getIn();
+  }
+  else{
+    getOut();
+  }
+  
+}
+var btn = document.getElementById("saveAge").addEventListener("click",mainFunc);
+var h1_first = document.getElementById("h1_").addEventListener("mouseover",getGreen);
+
+// part2
+var arrayName = []
+function getLastName(){
+  var userLastName = document.getElementById("lastName")
+  return userLastName.value;
+}
+function getFirstName(){
+  var userFirstName = document.getElementsByClassName("firstName")
+  arrayName.push(userFirstName)
+
+  return userFirstName.value;
+}
+
+
+
